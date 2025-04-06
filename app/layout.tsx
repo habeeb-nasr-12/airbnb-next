321aad81924b74e593e721cb5f8ec82a2a1dff7d
Habeeb <habebenasr4@gmail.com>
2025-04-06 10:51:50 +0200

    add SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <Navbar />
           {children}
+          <SpeedInsights />
         </AppRouterCacheProvider>
       </body>
     </html>
